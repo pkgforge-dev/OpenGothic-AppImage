6bc928c74cc4972c955b6658738aa9cd0d85b84a
Link4Electronics <link4electronics@gmail.com>
2026-05-01 08:42:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,8 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cmake \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"

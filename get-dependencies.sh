b4469bde3b64ea22adb37d29c803c5981b307627
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:11:07 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,10 +16,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Building OpenGothic..."
 echo "---------------------------------------------------------------"
 mkdir -p ./AppDir/bin
